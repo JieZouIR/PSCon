b902b5bd71fa51acc326cc27bc4c8d89f77df989
Shuxi Han <501560987@qq.com>
2024-06-08 23:04:04 +0800

    rm temp

--- temp/json2str.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('conversation_en.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
-    for item in data:
-        item['conversation'] = str(item['conversation'])
-
-with open('conversation_en_croissant_data.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
